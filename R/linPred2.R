c1f4afc873878dff5a039333a475a471d1af8fa4
Ken Kellner <ken@kenkellner.com>
2023-03-14 20:01:40 -0400

    Optionally use model.matrix version of names

--- R/linPred2.R
+++ R/linPred2.R
@@ -230,10 +230,16 @@ class(linPred2) <- "model_macro"
 # Generate a code block with parameter priors for a given formula and 
 # corresponding dataset
 # Fixes some parameter values at 0 if necessary (i.e., reference levels for factors)
-makePriorsFromFormula <- function(formula, data, prior, prefix){ 
+makePriorsFromFormula <- function(formula, data, prior, prefix, modMatNames=FALSE){ 
 
   par_struct <- makeParameterStructure(formula, data)
+  # Matching structure with the model matrix version of the names
+  # Plugged in later if modMatNames = TRUE
+  par_mm <- makeParameterStructureModMatNames(formula, data)
+
   par_names <- getParametersForLP(names(par_struct), prefix)
+
+  getParametersForLP(colnames(model.matrix(formula, data)), prefix)
 
   all_priors <- lapply(1:length(par_struct), function(i){
 
@@ -249,28 +255,76 @@ makePriorsFromFormula <- function(formula, data, prior, prefix){
       bracket <- paste0("[",paste(inds[j,], collapse=","),"]")
       node <- str2lang(paste0(par_names[i], bracket))
       if(val){
-        substitute(LHS ~ PRIOR, list(LHS=node, PRIOR=prior))
+        if(modMatNames){
+          alt_par <- str2lang(paste0(prefix, par_mm[[i]][t(inds[j,])]))
+          embedLinesInCurlyBrackets(
+            list(substitute(LHS <- ALT, list(LHS=node, ALT=alt_par)),
+                 substitute(ALT ~ PRIOR, list(ALT=alt_par, PRIOR=prior)))
+          )
+        } else {
+          substitute(LHS ~ PRIOR, list(LHS=node, PRIOR=prior))
+        }
       } else {
         substitute(LHS <- 0, list(LHS=node))
       }
     })
   })
   all_priors <- unlist(all_priors)
+  # unroll interior brackets
+  all_priors <- unlist(lapply(all_priors, function(x){
+    if(x[[1]] == "{") x <- as.list(x)[2:length(x)]
+    x
+  }))
+  
   embedLinesInCurlyBrackets(all_priors)
 }
+
+# Organize model.matrix() version of parameter names into an
+# identical data structure to makeParameterStructure()
+# So they can be matched if required
+makeParameterStructureModMatNames <- function(formula, data){
+ 
+  # Generate placeholder structure containing all 0s
+  empty_structure <- makeEmptyParameterStructure(formula, data)
+
+  pars_full <- colnames(model.matrix(formula, data))
+  pars_sep <- strsplit(pars_full, ":")
+  pars_full <- gsub("(","", pars_full, fixed=TRUE)
+  pars_full <- gsub(")", "", pars_full, fixed=TRUE)
+  pars_full <- gsub(":", ".", pars_full, fixed=TRUE)
+  
+  # Replace elements of parameter structure for which we actually estimate
+  # a parameter with 1
+  lapply(empty_structure, function(x){
+    for (i in 1:length(pars_sep)){
+      ind <- t(pars_sep[[i]])
+      if(length(dim(x)) == length(ind) & all(ind %in% unlist(dimnames(x)))){
+        x[ind] <- pars_full[i]
+      }
+    }
+    drop(x)
+  })
+}
+
 
 
 #' @export
 priors2 <- list(process=function(code, .constants, .env=env){
-  form <- nimbleMacros:::getRHS(code)[1:2][[2]]
+  form <- getRHS(code)[1:2][[2]]
   if(form[[1]] != quote(`~`)) form <- c(quote(`~`),form) 
   form <- as.formula(form)  
   dat <- makeDummyDataFrame(form, .constants)
 
-  prefix <- nimbleMacros:::getLHS(code)
-  priors <- nimbleMacros:::getRHS(code)[[3]]
+  modMatNames <- getRHS(code)$modMatNames
+  if(is.null(modMatNames)){
+    modMatNames <- FALSE
+  }
+  prefix <- getLHS(code)
+  priors <- getRHS(code)[[3]]
 
-  out <- makePriorsFromFormula(form, dat, priors, prefix=as.character(deparse(prefix)))
+  out <- makePriorsFromFormula(form, dat, priors, 
+                               prefix=as.character(deparse(prefix)),
+                               modMatNames = modMatNames)
   
   list(code=out, constants=.constants)
 })
