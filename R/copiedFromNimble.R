de92c8d6807e509d38e076ad78bf3f5ead8c8321
Ken Kellner <ken@kenkellner.com>
2023-03-15 13:15:08 -0400

    Fix some bugs with extracting indices from formulas

--- R/copiedFromNimble.R
+++ R/copiedFromNimble.R
@@ -1,12 +1,5 @@
 # Functions copied from the nimble package
 
-# Determine if a piece of code contains a '['
-hasBracket <- function(code) {
-    if(length(code) < 2) return(FALSE)
-    if(code[[1]] == '[') return(TRUE)
-    FALSE
-}
-
 embedLinesInCurlyBrackets <- function(lines) {
   as.call(c(list(quote(`{`)), lines))
 }
