32790c547950728a82663d369496f7b0865ac860
Ken Kellner <ken@kenkellner.com>
2026-06-11 10:36:06 -0400

    Don't enable macro comments by default

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   #message("Enabling model macros in nimbleOptions")
   nimble::nimbleOptions(enableMacros = TRUE)
-  nimble::nimbleOptions(enableMacroComments = TRUE)
   invisible()
 }
